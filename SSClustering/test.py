5cd32f6f0d0a13ba549b31c971efe9deb50cdc91
petrosgeor <petrosgw00@gmail.com>
2023-12-11 09:49:15 +0200

    message

--- SSClustering/test.py
+++ SSClustering/test.py
@@ -1,22 +1,13 @@
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.datasets import load_digits
+from sklearn.manifold import SpectralEmbedding
 
-'''
-x = torch.randn(10, 100)
-A = torch.randn(10, 20, 100)
-'''
 
-x = torch.tensor([[1,2,3],[10,20,30]])
 
-A = torch.tensor([[[1,2,3],
-                   [-71,-2,-3],
-                   [100, 100, 100]],
-                  [[-1, -2, -3],
-                   [1, 22, 3],
-                   [35, 64, 1234]]])
 
-x_transposed = x.unsqueeze(1)
+x = torch.randn((100, 100))
+print(torch.where(x >= 0.5))
 
-result = torch.bmm(A, x_transposed.transpose(1,2))
 
