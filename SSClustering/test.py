2cb918d52cc4b8ee09c34eb95134a37b2959268c
petrosgeor <petrosgw00@gmail.com>
2023-12-12 17:07:14 +0200

    message

--- SSClustering/test.py
+++ SSClustering/test.py
@@ -2,35 +2,37 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_digits
-from sklearn.manifold import SpectralEmbedding
+from sklearn.manifold import MDS
 
 
 
-mean = 35
-std = 5
+# mean = 35
+# std = 5
 
 
-x = np.floor(np.random.normal(mean, std, 60000))
-plt.hist(x, bins=60)
-plt.xlabel('Values')
-plt.ylabel('Frequence')
-plt.title('Histogram Example')
-plt.grid(True)
-plt.show()
+# x = np.floor(np.random.normal(mean, std, 60000))
+# plt.hist(x, bins=60)
+# plt.xlabel('Values')
+# plt.ylabel('Frequence')
+# plt.title('Histogram Example')
+# plt.grid(True)
+# plt.show()
 
 
-x = np.arange(0, 50)
-n = 50
+# x = np.arange(0, 50)
+# n = 50
 
-def f(x, n):
-    return (x * (x-1))/(n * (n-1))
+# def f(x, n):
+#     return (x * (x-1))/(n * (n-1))
     
 
-y = f(x, n)
-plt.scatter(x, y)
-plt.xlabel('number of neighbors with the same label')
-plt.ylabel('fraction of correct links')
-plt.grid(True)
-plt.show()
+# y = f(x, n)
+# plt.scatter(x, y)
+# plt.xlabel('number of neighbors with the same label')
+# plt.ylabel('fraction of correct links')
+# plt.grid(True)
+# plt.show()
 
-
+x = np.random.randn(2000, 100)
+mds = MDS()
+mds.fit(x)
--- SSClustering/training.py
+++ SSClustering/training.py
@@ -169,7 +169,7 @@ def create_SCAN_dl_LINKED_dl(net: Network, deterministic_neighbors = False) -> t
             neighbor_indices = find_indices_of_closest_embeddings(embeddings, distance='cosine', n_neighbors=20)
     elif deterministic_neighbors == True:
         #neighbor_indices = deterministic_closest_indices(Ids=dataset.Ids, n_neighbors=50, n_correct=35)
-        neighbor_indices = probabilistic_closest_indices(Ids=dataset.Ids, n_neighbors=50, n_correct_mean=35)
+        neighbor_indices = probabilistic_closest_indices(Ids=dataset.Ids, n_neighbors=20, n_correct_mean=12)
 
     scan_dataset = SCANdatasetWithNeighbors(data=dataset.data, Ids=dataset.Ids, neighbor_indices=neighbor_indices)
     scan_dataloader = DataLoader(scan_dataset, batch_size=1200, shuffle=True, num_workers=2)
@@ -190,7 +190,7 @@ def train_clustering_network(num_epochs=2, t_contrastive=0.5, consider_links: bo
     clusternet.to(device)
     id_aug = Identity_Augmentation()
     aug_clr = SimCLRaugment()
-    scan_dataloader, linked_dataloader = create_SCAN_dl_LINKED_dl(net=clusternet, deterministic_neighbors=False)
+    scan_dataloader, linked_dataloader = create_SCAN_dl_LINKED_dl(net=clusternet, deterministic_neighbors=True)
     n_neighbors = scan_dataloader.dataset.n_neighbors
     n_classes = (torch.unique(scan_dataloader.dataset.Ids)).numel()
     ####
