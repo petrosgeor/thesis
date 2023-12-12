673824bc36ba6238c4df2fc23d76761277cc6d9c
petrosgeor <petrosgw00@gmail.com>
2023-12-12 15:49:34 +0200

    message

--- SSClustering/test.py
+++ SSClustering/test.py
@@ -7,10 +7,15 @@ from sklearn.manifold import SpectralEmbedding
 
 
 mean = 35
-std = 10
+std = 5
 
 
-x = np.floor(np.random.normal(mean, std, 1))
-
+x = np.floor(np.random.normal(mean, std, 60000))
+plt.hist(x, bins=60)
+plt.xlabel('Values')
+plt.ylabel('Frequence')
+plt.title('Histogram Example')
+plt.grid(True)
+plt.show()
 
 
