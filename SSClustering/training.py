4b0fde14ee7b7fa18982e91917f753f4f2456981
petrosgeor <petrosgw00@gmail.com>
2023-12-15 16:51:43 +0200

    message

--- SSClustering/training.py
+++ SSClustering/training.py
@@ -210,8 +210,8 @@ def train_clustering_network(num_epochs=2, t_contrastive=0.5, consider_links: bo
         contrastivemodel = test2.ContrastiveModel(backbone=backbone)
         file_path = 'NeuralNets/simclr_cifar10.pth'
         checkpoint = torch.load(file_path)
-        print('111111111111111111111111111111111111111111')
         contrastivemodel.load_state_dict(checkpoint)
+        print('111111111111111111111111111111111111111111')
         clusternet = test2.ClusteringModel(backbone={'backbone': contrastivemodel.backbone, 'dim': contrastivemodel.backbone_dim}, nclusters=10)
 
     if pretrained == 'no_links':
