9bc7fb9c5e317e081be5b4ffe3c50a3265bcc676
petrosgeor <petrosgw00@gmail.com>
2023-12-17 16:56:05 +0200

    message

--- SSClustering/training.py
+++ SSClustering/training.py
@@ -490,7 +490,7 @@ def run_pretraining_function():
     else:
         return 'no pretraining will take place'
 
-train_clustering_network3(num_epochs=101, n_neighbors=20, consider_distnaces=False, num_links=100000)
+train_clustering_network3(num_epochs=101, n_neighbors=20, consider_distnaces=False, num_links=50000)
 
 
 # scan_dataloader = train_clustering_network(num_epochs=300, t_contrastive=0.5, consider_links = True, n_neighbors=20,
