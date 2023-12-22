00810580b73140048c5d8468325477de6346a09e
petrosgeor <petrosgw00@gmail.com>
2023-12-22 20:39:42 +0200

    Message

--- SCAN_CIFAR/test.py
+++ SCAN_CIFAR/test.py
@@ -1,13 +1,23 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import torch
-import torchvision
 from models import * 
 from lightly.transforms.simclr_transform import SimCLRTransform
 
 
 
+
+x = np.linspace(-6, 10, 10000)
+
+def my_tanh(x, a=1):
+    numerator = np.exp(a*x) - np.exp(-a*x)
+    denominator = np.exp(a*x) + np.exp(-a*x)
+    return (numerator/denominator + 1)/2
+
+y = my_tanh(x, a=5)
+
+plt.plot(x, y)
+plt.show()
 
 
 
