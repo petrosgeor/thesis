47a5945ebe9b56af288c00ced3030d5bf1c90b96
petrosgeor <petrosgw00@gmail.com>
2023-12-22 20:43:45 +0200

    Message

--- SCAN_CIFAR/test.py
+++ SCAN_CIFAR/test.py
@@ -7,17 +7,6 @@ from lightly.transforms.simclr_transform import SimCLRTransform
 
 
 
-x = np.linspace(-6, 10, 10000)
-
-def my_tanh(x, a=1):
-    numerator = np.exp(a*x) - np.exp(-a*x)
-    denominator = np.exp(a*x) + np.exp(-a*x)
-    return (numerator/denominator + 1)/2
-
-y = my_tanh(x, a=5)
-
-plt.plot(x, y)
-plt.show()
 
 
 
