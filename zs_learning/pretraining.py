b64d921560f77bcdbca464e0cdc360973435130b
petrosgeor <petrosgw00@gmail.com>
2023-12-23 10:08:29 +0200

    Message

--- zs_learning/pretraining.py
+++ zs_learning/pretraining.py
@@ -93,44 +93,14 @@ class ContrastiveModel(pl.LightningModule):
     
 
 
-class SimCLRModel(pl.LightningModule):
-    def __init__(self):
-        super().__init__()
 
-        # create a ResNet backbone and remove the classification head
-        resnet = torchvision.models.resnet18()
-        self.backbone = nn.Sequential(*list(resnet.children())[:-1])
-
-        hidden_dim = resnet.fc.in_features
-        self.projection_head = SimCLRProjectionHead(hidden_dim, hidden_dim, 128)
-
-        self.criterion = NTXentLoss()
-
-    def forward(self, x):
-        h = self.backbone(x).flatten(start_dim=1)
-        z = self.projection_head(h)
-        return z
-
-    def training_step(self, batch, batch_idx):
-        (x0, x1), _, _ = batch
-        z0 = self.forward(x0)
-        z1 = self.forward(x1)
-        loss = self.criterion(z0, z1)
-        return loss
-
-    def configure_optimizers(self):
-        optim = torch.optim.SGD(
-            self.parameters(), lr=6e-2, momentum=0.9, weight_decay=5e-4
-        )
-        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optim, max_epochs)
-        return [optim], [scheduler]
 
 # model = SimCLRModel()
 # trainer = pl.Trainer(max_epochs=max_epochs, devices=1, accelerator="gpu")
 #iter(dataloader_train_simclr)
 # trainer.fit(model, dataloader_train_simclr)
 
-model = ContrastiveModel(backbone=resnet34())
+model = ContrastiveModel(backbone=resnet18())
 
 for batch in dataloader_train_simclr:
     break
