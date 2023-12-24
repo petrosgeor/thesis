5e8c412bb2e7eb9bffd22214f6de6b46e9476eca
petrosgeor <petrosgw00@gmail.com>
2023-12-24 23:46:36 +0200

    Message

--- zs_learning/pretraining.py
+++ zs_learning/pretraining.py
@@ -38,7 +38,9 @@ memory_bank_size = 2000
 
 path_to_data = set_AwA2_dataset_path() + 'JPEGImages/'
 
-transform = MoCoV2Transform(input_size=input_size)
+transform = MoCoV2Transform(input_size=input_size,
+                            gaussian_blur=0.0,
+                            )
 
 test_transform = torchvision.transforms.Compose(
     [
